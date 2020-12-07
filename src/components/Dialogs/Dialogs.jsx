4b58d391fdccda74031b0bdbd032a1fa1e7d53fc
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2020-12-07 23:06:54 +0300

    массивы в сообщениях и .map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,31 +5,42 @@ import OneMessage from "./OneMessage"
 
 
 const Dialogs = () => {
+
+    let dialogs = [
+        {id: 1, name: 'Bobik'},
+        {id: 2, name: 'Tyzik'},
+        {id: 3, name: 'Almaz'},
+        {id: 4, name: 'Krysa'},
+        {id: 5, name: 'Reks'},
+        {id: 6, name: 'Tobik'},
+        {id: 7, name: 'Korg'},
+        {id: 8, name: 'Azaman'},
+        {id: 9, name: 'Belyj'},
+    ]
+
+    let messagesData = [
+        {id:1, message:'Hi, how are you?'},
+        {id:2, message:'Ok'},
+        {id:3, message:'Go eat sheet to garden!'},
+        {id:4, message:'Go eat sheet to garden!!'},
+        {id:5, message:'Go eat sheet to garden!!!'},
+        {id:6, message:'Go eat sheet to garden!!!!'},
+        {id:7, message:'Go eat sheet to garden!!!!!'},
+        {id:8, message:'Go eat sheet to garden!!!!!!'},
+    ]
+
+    let dialogsElements = dialogs.map ( dialog => <Dialog name={dialog.name} id={dialog.id} /> )
+    let messagesNEWARRAY = messagesData.map ( t => <OneMessage text={t.message} id={t.id}/> )
+
+
+
     return (
         <div className={classes.Messages}>
 
             <h1 className='CaptionPages'>Chat with your dogs-friends</h1>
-
             <div className={classes.Dialogs}>
-
-                <div className={classes.DialogItem}>
-                    <Dialog name="Bobik" id="1"/>
-                    <Dialog name="Tyzik" id="2"/>
-                    <Dialog name="Almaz" id="3"/>
-                    <Dialog name="Krysa" id="4"/>
-                    <Dialog name="Reks" id="5"/>
-                    <Dialog name="Tobik" id="6"/>
-                    <Dialog name="Korg" id="7"/>
-                    <Dialog name="Azaman" id="8"/>
-                    <Dialog name="Belyj" id="9"/>
-                </div>
-
-                <div className={classes.MessagesOfItem}>
-                    <OneMessage text='Hi, how are you?' />
-                    <OneMessage text='Ok' />
-                    <OneMessage text='Go eat sheet to garden!' />
-                </div>
-
+                <div className={classes.DialogItem}>{dialogsElements}</div>
+                <div className={classes.MessagesOfItem}>{messagesNEWARRAY}</div>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,18 +3,30 @@ import '../../../App.css'
 import classes from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = (props) => {
+const MyPosts = () => {
+    let postsData = [
+        {id:1, posttext:'Hi, how are you?', likes:10 },
+        {id:2, posttext:'Privet, Medved!', likes:114 },
+        {id:3, posttext:'What do you do, tommorow?', likes:5 },
+        {id:4, posttext:'You nice man?', likes:4 },
+        {id:5, posttext:'I like Picnic', likes:107 },
+        {id:6, posttext:'I learn 16 lesson It-camasutra!', likes:54 },
+        {id:7, posttext:'Props is krytaja vesch!', likes:42 },
+    ]
+
+
+
     return (
         <div className={classes.posts}>
             <h2>My posts</h2>
             <div>New post</div>
-            <Post message='Hi, how are you?' likes='10'/>
-            <Post message='Privet, Medved!' likes='114'/>
-            <Post message='What do you do, tommorow?' likes='5'/>
-            <Post message='You nice man?' likes='4'/>
-            <Post message='I like Picnic' likes='107'/>
-            <Post message='I learn 16 lesson It-camasutra!' likes='54'/>
-            <Post message='Props is krytaja vesch!' likes='42'/>
+            <Post message={postsData[0].posttext} likes={postsData[0].likes}/>
+            <Post message={postsData[1].posttext} likes={postsData[1].likes}/>
+            <Post message={postsData[2].posttext} likes={postsData[2].likes}/>
+            <Post message={postsData[3].posttext} likes={postsData[3].likes}/>
+            <Post message={postsData[4].posttext} likes={postsData[4].likes}/>
+            <Post message={postsData[5].posttext} likes={postsData[5].likes}/>
+            <Post message={postsData[6].posttext} likes={postsData[6].likes}/>
         </div>
     )
 }

d26e4fe85b488775d667c446252c3bbe179c6895
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2021-01-04 21:54:34 +0300

    refactoring input obj store  OOP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals'
-import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
@@ -12,8 +12,8 @@ let rerenderEntireTree = (state) => {
         <React.StrictMode>
             <BrowserRouter>
                 <App state={state}
-                     addPost={addPost}
-                     updateNewPostText={updateNewPostText}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}
                 />
             </BrowserRouter>
         </React.StrictMode>,
@@ -21,9 +21,9 @@ let rerenderEntireTree = (state) => {
     );
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,66 +1,74 @@
-let rerenderEntireTree = () => {
-    console.log('ddgdgdgdgd')
-}
+let store = {
 
-let state = {
-    profilePage: {
-        posts : [
-        {id: 1, posttext: 'Hi, how are you?', likes: 10},
-        {id: 2, posttext: 'Privet, Medved!', likes: 114},
-        {id: 3, posttext: 'What do you do, tommorow?', likes: 5},
-        {id: 4, posttext: 'You nice man?', likes: 4},
-        {id: 5, posttext: 'I like Picnic', likes: 107},
-        {id: 6, posttext: 'I learn 16 lesson It-camasutra!', likes: 54},
-        {id: 7, posttext: 'Props is krytaja vesch!', likes: 42}
-    ],
-        newPostText: ''
+    _state : {
+        profilePage: {
+            posts : [
+                {id: 1, posttext: 'Hi, how are you?', likes: 10},
+                {id: 2, posttext: 'Privet, Medved!', likes: 114},
+                {id: 3, posttext: 'What do you do, tommorow?', likes: 5},
+                {id: 4, posttext: 'You nice man?', likes: 4},
+                {id: 5, posttext: 'I like Picnic', likes: 107},
+                {id: 6, posttext: 'I learn 16 lesson It-camasutra!', likes: 54},
+                {id: 7, posttext: 'Props is krytaja vesch!', likes: 42}
+            ],
+            newPostText: ''
+        },
+        dialogs: [
+            {id: 1, name: 'Bobik'},
+            {id: 2, name: 'Snezhok'},
+            {id: 3, name: 'Almaz'},
+            {id: 4, name: 'Krysa'},
+            {id: 5, name: 'Amur'},
+            {id: 6, name: 'Tobik'},
+            {id: 7, name: 'Korg'},
+            {id: 8, name: 'Azaman'},
+            {id: 9, name: 'Belyj'},
+        ],
+        messages: [
+            {id:1, message:'Hi, how are you?'},
+            {id:2, message:'Ok'},
+            {id:3, message:'Go eat sheet to garden!'},
+            {id:4, message:'Go eat sheet to garden!!'},
+            {id:5, message:'Go eat sheet to garden!!!'},
+            {id:6, message:'Go eat sheet to garden!!!!'},
+            {id:7, message:'Go eat sheet to garden!!!!!'},
+            {id:8, message:'Go eat sheet to garden!!!!!!'},
+        ]
     },
-    dialogs: [
-        {id: 1, name: 'Bobik'},
-        {id: 2, name: 'Snezhok'},
-        {id: 3, name: 'Almaz'},
-        {id: 4, name: 'Krysa'},
-        {id: 5, name: 'Amur'},
-        {id: 6, name: 'Tobik'},
-        {id: 7, name: 'Korg'},
-        {id: 8, name: 'Azaman'},
-        {id: 9, name: 'Belyj'},
-    ],
-    messages: [
-        {id:1, message:'Hi, how are you?'},
-        {id:2, message:'Ok'},
-        {id:3, message:'Go eat sheet to garden!'},
-        {id:4, message:'Go eat sheet to garden!!'},
-        {id:5, message:'Go eat sheet to garden!!!'},
-        {id:6, message:'Go eat sheet to garden!!!!'},
-        {id:7, message:'Go eat sheet to garden!!!!!'},
-        {id:8, message:'Go eat sheet to garden!!!!!!'},
-    ]
-}
 
-window.state = state
+    getState() {
+        return this._state;
+    },
 
-export const addPost = () => {
+    _callSubscriber() {
+        console.log('random')
+    },
 
-    let postsLength = state.profilePage.posts.length++
+    addPost() {
 
-    let newPost = {
-        id: postsLength,
-        posttext: state.profilePage.newPostText,
-        likes: 0
+        let postsLength = store._state.profilePage.posts.length++
+
+        let newPost = {
+            id: postsLength,
+            posttext: store._state.profilePage.newPostText,
+            likes: 0
+        }
+        store._state.profilePage.posts.push(newPost)
+        store._state.profilePage.newPostText = ''
+        store._callSubscriber(store._state)
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer
     }
-     state.profilePage.posts.push(newPost)
-     state.profilePage.newPostText = ''
-     rerenderEntireTree(state)
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
+window.store = store
 
-export default state;
+export default store;
